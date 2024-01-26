f67d5fb3f1ef1331de7a9bf81ce8d603e9276d98
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2024-01-26 22:52:55 +0100

    message

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,12 +4,12 @@ import HTML from "../assets/html.png";
 import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
-import Photoshop from "../assets/adobe-photoshop.png";
-import CorelDraw from "../assets/CorelDraw.png";
+// import Photoshop from "../assets/adobe-photoshop.png";
+// import CorelDraw from "../assets/CorelDraw.png";
+// import Vmix from "../assets/vMix4.png";
 import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
 import C_Sharp from "../assets/C-Sharp.png";
-import Vmix from "../assets/vMix4.png";
 import DotNet from "../assets/Dotnet.png";
 
 const Skills = () => {
@@ -25,65 +25,87 @@ const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-5">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
             <p className="my-4">HTML</p>
+            <div className="relative w-full h-4 bg-gray-300 rounded-lg">
+              <div
+                className="absolute top-0  border left-0 h-full bg-blue-900 rounded-lg"
+                style={{ width: "calc(50% - 2px)" }}
+              ></div>
+            </div>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-5">
             <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
             <p className="my-4">CSS</p>
+            <div className="relative w-full h-4 bg-gray-300 rounded-lg">
+              <div
+                className="absolute top-0  border left-0 h-full bg-blue-900 rounded-lg"
+                style={{ width: "calc(50% - 2px)" }}
+              ></div>
+            </div>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="TAILWIND icon" />
+            <p className="my-4">TAILWIND</p>
+            <div className="relative w-full h-4 bg-gray-300 rounded-lg">
+              <div
+                className="absolute top-0  border left-0 h-full bg-blue-900 rounded-lg"
+                style={{ width: "calc(60% - 2px)" }}
+              ></div>
+            </div>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-5">
             <img
               className="w-20 mx-auto"
               src={JavaScript}
               alt="JAVASCRIPT icon"
             />
             <p className="my-4">JAVASCRIPT</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={ReactImg} alt="REACT icon" />
-            <p className="my-4">REACT</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={GitHub} alt="GITHUB icon" />
-            <p className="my-4">GITHUB</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-40 mx-auto" src={DotNet} alt=".Net icon" />
-            <p className="my-4">.Net</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={C_Sharp} alt=".C_Sharp icon" />
-            <p className="my-4">C_Sharp</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-40 mx-auto" src={Vmix} alt="VMIX icon" />
-            <p className="my-4">VMIX</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Tailwind} alt="TAILWIND icon" />
-            <p className="my-4">TAILWIND</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-40 mx-auto"
-              src={Photoshop}
-              alt="PHOTOSHOP icon"
-            />
-            <p className="my-4">PHOTOSHOP</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-20 mx-auto"
-              src={CorelDraw}
-              alt="CORELDRAW icon"
-            />
-            <p className="my-4">CORELDRAW</p>
             <div className="relative w-full h-4 bg-gray-300 rounded-lg">
               <div
                 className="absolute top-0  border left-0 h-full bg-blue-900 rounded-lg"
-                style={{ width: "calc(50% - 2px)" }}
+                style={{ width: "calc(60% - 2px)" }}
+              ></div>
+            </div>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-5">
+            <img className="w-20 mx-auto" src={ReactImg} alt="REACT icon" />
+            <p className="my-4">REACT</p>
+            <div className="relative w-full h-4 bg-gray-300 rounded-lg">
+              <div
+                className="absolute top-0  border left-0 h-full bg-blue-900 rounded-lg"
+                style={{ width: "calc(60% - 2px)" }}
+              ></div>
+            </div>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-5">
+            <img className="w-20 mx-auto" src={GitHub} alt="GITHUB icon" />
+            <p className="my-4">GITHUB</p>
+            <div className="relative w-full h-4 bg-gray-300 rounded-lg">
+              <div
+                className="absolute top-0  border left-0 h-full bg-blue-900 rounded-lg"
+                style={{ width: "calc(70% - 2px)" }}
+              ></div>
+            </div>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-5">
+            <img className="w-40 mx-auto" src={DotNet} alt=".Net icon" />
+            <p className="my-4">.Net</p>
+            <div className="relative w-full h-4 bg-gray-300 rounded-lg">
+              <div
+                className="absolute top-0  border left-0 h-full bg-blue-900 rounded-lg"
+                style={{ width: "calc(80% - 2px)" }}
+              ></div>
+            </div>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-5">
+            <img className="w-20 mx-auto" src={C_Sharp} alt=".C_Sharp icon" />
+            <p className="my-4">C_Sharp</p>
+            <div className="relative w-full h-4 bg-gray-300 rounded-lg">
+              <div
+                className="absolute top-0  border left-0 h-full bg-blue-900 rounded-lg"
+                style={{ width: "calc(80% - 2px)" }}
               ></div>
             </div>
           </div>
